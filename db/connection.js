c36cbb052ba8c323ea3d7bd70cb0d1f9def1ed78
cabrinilm <luca_cabrinimm@hotmail.com>
2025-01-31 16:45:18 +0000

    move pg-format to dependencies

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
@@ -9,18 +9,11 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config = {};
+const config =
+  ENV === "production"
+    ? { connectionString: process.env.DATABASE_URL, max: 2 }
+    : {}; 
 
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-module.exports = new Pool(config);
-
-
-
-const db = new Pool()
-
-
+const db = new Pool(config);
 
 module.exports = db;
