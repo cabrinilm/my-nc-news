0e7064650ef1a6446b8cacd097b9aa5bcf54d18b
cabrinilm <luca_cabrinimm@hotmail.com>
2025-02-08 12:53:31 +0000

    update listen.js

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
-const app = require('./app')
+const app = require('./app');
 
-const { PORT = 9090 } = process.env;
+const PORT = process.env.PORT || 10000; 
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}!`)
-})
+    console.log(`Server listening on port ${PORT}!`);
+});
