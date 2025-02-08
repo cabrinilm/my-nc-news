1720e6743386d896aa3d8aa9afbce9a1d3433303
cabrinilm <luca_cabrinimm@hotmail.com>
2025-02-08 15:19:47 +0000

    new connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,17 @@
-const { Pool } = require("pg");
-const ENV = process.env.NODE_ENV || "development";
-
-require("dotenv").config({
+const { Pool } = require('pg');
+const ENV = process.env.NODE_ENV || 'development';
+require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
-const config =
-  ENV === "production"
-    ? { connectionString: process.env.DATABASE_URL, max: 2 }
-    : {}; 
-
-const db = new Pool(config);
-
-module.exports = db;
+module.exports = new Pool(config);
